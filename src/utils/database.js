fd5dba73d8a4f40e766df8de6eb9d177faa49775
Juan Arbelaez <juanalvaro77hotmail.com>
2023-05-20 11:40:21 -0500

    Corrigiendo la coexion con database

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require("sequelize");
 require("dotenv").config();
 
-/*const db = new Sequelize({
+const db = new Sequelize({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
@@ -9,17 +9,9 @@ require("dotenv").config();
     password: process.env.DB_PASSWORD,
     dialect: "postgres",
     dialectOptions: { ssl: { require: true, rejectUnauthorized: false }}
-});*/
-
-const db = new Sequelize({
-    host: "localhost",
-    database: "relations",
-    port: 5432,
-    username: "postgres",
-    password: "root",
-    dialect: "postgres",
-    //dialectOptions: { ssl: { require: true, rejectUnauthorized: false }}
 });
+
+
 
 
 module.exports = db;
